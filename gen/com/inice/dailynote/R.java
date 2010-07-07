2bc4b658c54ea3cba170f00a070633f5e8e4af11
Chongsawad <Chongsawad252@gmail.com>
2010-07-07 21:54:50 +0700

    new Window

--- gen/com/inice/dailynote/R.java
+++ gen/com/inice/dailynote/R.java
@@ -13,17 +13,23 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
         public static final int nature=0x7f020001;
+        public static final int paper=0x7f020002;
+        public static final int paper_clipped=0x7f020003;
+        public static final int paper_yellow=0x7f020004;
     }
     public static final class id {
         public static final int bNext=0x7f06000b;
         public static final int bPrev=0x7f060009;
         public static final int bToday=0x7f06000a;
+        public static final int btDone=0x7f06000e;
         public static final int dateButtonBar=0x7f060008;
         public static final int dateLayout=0x7f060006;
         public static final int dateView=0x7f060005;
         public static final int dateWrapper=0x7f060007;
+        public static final int headNote=0x7f06000d;
         public static final int inputNum=0x7f060003;
         public static final int note=0x7f06000c;
+        public static final int title=0x7f06000f;
         public static final int unitChange=0x7f060004;
         public static final int unitChangeLayout=0x7f060002;
         public static final int webViewLayout=0x7f060001;
@@ -31,6 +37,7 @@ public final class R {
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int note_edit=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/com/inice/dailynote/DailyNote.java
+++ src/com/inice/dailynote/DailyNote.java
@@ -1,23 +1,23 @@
 package com.inice.dailynote;
 
+import java.util.logging.Logger;
+
 import android.app.Activity;
-import android.inputmethodservice.InputMethodService;
-import android.inputmethodservice.KeyboardView;
-import android.inputmethodservice.KeyboardView.OnKeyboardActionListener;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.view.inputmethod.InputConnection;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 
 public class DailyNote extends Activity {
-	private TextView inputNum, dateView,note;
-	private Button bkg;
-	final static int DATE_DIALOG_ID = 0;
+	private TextView inputNum, dateView,newNote;
+	private Button bkg,dButton;
+	private final static int DATE_DIALOG_ID = 0;
+    private static final int ACTIVITY_CREATE=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +26,20 @@ public class DailyNote extends Activity {
         bkg = (Button) findViewById(R.id.unitChange);
         inputNum = (EditText) findViewById(R.id.inputNum);
         dateView = (TextView) findViewById(R.id.dateView);
-        note = (EditText) findViewById(R.id.note);
         
         setContentView(R.layout.main);
-    }    
+        
+        newNote = (TextView) findViewById(R.id.note);
+        
+        newNote.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				createNewNote();
+			}
+		});
+    }
+    private void createNewNote(){
+    	Intent i = new Intent(this, NoteEdit.class);
+    	startActivity(i);
+    }
 }
