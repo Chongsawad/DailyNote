68411dbbd03fd8ff1ebcf778b6a06bc24c58b1d1
Chongsawad <Chongsawad252@gmail.com>
2010-07-08 16:52:26 +0700

    unitChange

--- gen/com/inice/dailynote/R.java
+++ gen/com/inice/dailynote/R.java
@@ -21,23 +21,27 @@ public final class R {
         public static final int bNext=0x7f06000b;
         public static final int bPrev=0x7f060009;
         public static final int bToday=0x7f06000a;
-        public static final int btDone=0x7f06000e;
+        public static final int date=0x7f060005;
         public static final int dateButtonBar=0x7f060008;
         public static final int dateLayout=0x7f060006;
-        public static final int dateView=0x7f060005;
+        public static final int dateString=0x7f06000e;
         public static final int dateWrapper=0x7f060007;
+        public static final int done=0x7f06000f;
         public static final int headNote=0x7f06000d;
         public static final int inputNum=0x7f060003;
         public static final int note=0x7f06000c;
-        public static final int title=0x7f06000f;
+        public static final int title=0x7f060010;
         public static final int unitChange=0x7f060004;
         public static final int unitChangeLayout=0x7f060002;
+        public static final int unitDone=0x7f060011;
+        public static final int unitList=0x7f060012;
         public static final int webViewLayout=0x7f060001;
         public static final int wrapper=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
         public static final int note_edit=0x7f030001;
+        public static final int unit_change=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/com/inice/dailynote/DailyNote.java
+++ src/com/inice/dailynote/DailyNote.java
@@ -1,8 +1,9 @@
 package com.inice.dailynote;
 
-import java.util.logging.Logger;
+import java.util.Calendar;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,34 +13,126 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-
 public class DailyNote extends Activity {
-	private TextView inputNum, dateView,newNote;
-	private Button bkg,dButton;
+	public StringBuilder s;
+	private TextView dateView,newNote;
+	private TextView bToday, bPrev, bNext;
+	private Button unitButton;
 	private final static int DATE_DIALOG_ID = 0;
-    private static final int ACTIVITY_CREATE=0;
-
+    private int cDay, cMonth, cYear;
+    private int vDay, vMonth, vYear;
+	private Calendar c;    
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        bkg = (Button) findViewById(R.id.unitChange);
-        inputNum = (EditText) findViewById(R.id.inputNum);
-        dateView = (TextView) findViewById(R.id.dateView);
-        
         setContentView(R.layout.main);
         
-        newNote = (TextView) findViewById(R.id.note);
+        c = Calendar.getInstance();
         
-        newNote.setOnClickListener(new View.OnClickListener() {
+        cDay = c.get(Calendar.DAY_OF_MONTH);
+        cMonth = c.get(Calendar.MONTH);
+        cYear = c.get(Calendar.YEAR);
+        
+        dateView = (TextView) findViewById(R.id.date);
+        
+        updateDate();
+        
+        dateView.setOnClickListener(new View.OnClickListener() {
 			
+			public void onClick(View v) {
+				showDialog(DATE_DIALOG_ID);
+			}
+		});
+        
+        bToday = (TextView) findViewById(R.id.bToday);
+        bToday.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				resetDate();
+				updateDate();
+			}
+		});
+        
+        bNext = (TextView) findViewById(R.id.bNext);
+        bNext.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				c.add(Calendar.DAY_OF_MONTH, 1);
+				updateDate();
+			}
+		});
+        
+        bPrev = (TextView) findViewById(R.id.bPrev);
+        bPrev.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				c.add(Calendar.DAY_OF_MONTH, -1);
+				updateDate();
+			}
+		});
+        
+        
+        newNote = (TextView) findViewById(R.id.note);
+        newNote.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				createNewNote();
 			}
 		});
+        
+        unitButton = (Button) findViewById(R.id.unitChange);
+        unitButton.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				unitChange();
+			}
+		});
     }
+    private void unitChange(){
+    	Intent u = new Intent(this, unitChange.class);
+    	startActivity(u);
+    }
+    
     private void createNewNote(){
     	Intent i = new Intent(this, NoteEdit.class);
     	startActivity(i);
     }
+    private void resetDate(){
+    	vDay = cDay;
+    	vMonth = cMonth;
+    	vYear = cYear;
+    	c.set(vYear, vMonth, vDay);
+    }
+    
+    private void updateDate(){
+    	vYear = c.get(Calendar.YEAR);
+    	vMonth = c.get(Calendar.MONTH);
+    	vDay = c.get(Calendar.DAY_OF_MONTH);
+    	s = new StringBuilder().append(vMonth + "/" + vDay + "/"+  vYear);
+    	dateView.setText(new StringBuilder().append(vMonth + "/" + vDay + "/"+  vYear));
+    }
+    
+    @Override
+    protected Dialog onCreateDialog(int id){
+    	switch (id) {
+		case DATE_DIALOG_ID:
+			{
+				return new DatePickerDialog(this,mDateList,vYear,vMonth,vDay);
+			}
+		}
+    	return null;
+    }
+    
+    private DatePickerDialog.OnDateSetListener mDateList = new DatePickerDialog.OnDateSetListener() {
+
+		public void onDateSet(android.widget.DatePicker view, int year,
+				int monthOfYear, int dayOfMonth) {
+			vYear = year;
+			vMonth = monthOfYear;
+			vDay = dayOfMonth;
+			c.set(vYear, vMonth, vDay);
+			updateDate();
+		}
+	};
 }
